b3445769b54bf15b13096283c8e70a214305d3fd
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-26 17:07:57 +0100

    Minor fixes to pass the tests

--- Memorization Tool/task/tool.py
+++ Memorization Tool/task/tool.py
@@ -5,39 +5,41 @@ flashcards = []
 
 
 def display_menu():
-    print("1. Add Flashcard")
+    print("1. Add flashcards")
     print("2. Practice flashcards")
     print("3. Exit")
 
 
 def add_flashcard():
-    print("1. Add a new flashcard")
-    print("2. Exit")
-    choice = input()
-    match choice:
-        case "1":
-            while True:
-                # guard against empty input
-                print("Question:")
-                question = input()
-                if question.strip() == "":
-                    continue
-                else:
-                    break
-            while True:
-                # guard against empty input
-                print("Answer:")
-                answer = input()
-                if answer.strip() == "":
-                    continue
-                else:
-                    break
-            fc = Flashcard(question, answer)
-            flashcards.append(fc)
-        case "2":
-            return
-        case _:
-            print(f"{choice} is not an option")
+    while True:
+        print("1. Add a new flashcard")
+        print("2. Exit")
+
+        choice = input()
+        match choice:
+            case "1":
+                while True:
+                    # guard against empty input
+                    print("Question:")
+                    question = input()
+                    if question.strip() == "":
+                        continue
+                    else:
+                        break
+                while True:
+                    # guard against empty input
+                    print("Answer:")
+                    answer = input()
+                    if answer.strip() == "":
+                        continue
+                    else:
+                        break
+                fc = Flashcard(question, answer)
+                flashcards.append(fc)
+            case "2":
+                return
+            case _:
+                print(f"{choice} is not an option")
 
 
 def practice_flashcards():
@@ -45,8 +47,7 @@ def practice_flashcards():
         print("There is no flashcard to practice!")
         return
     for fc in flashcards:
-        print("Question:")
-        print(fc.question)
+        print(f"Question: {fc.question}")
         print("Please press \"y\" to see the answer or press \"n\" to skip:")
         user_answer = input()
         if user_answer.strip() == "y":
